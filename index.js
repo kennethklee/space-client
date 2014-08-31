940233ac2d47d39ef974580dc4f214fe90b7fdcc
Kenneth Lee <kennethkl@gmail.com>
2014-08-31 04:02:01 +0000

    gulp generator

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var env = process.env.NODE_ENV || 'development',
-    app = require('express')(),
+    port = process.env.PORT || 4000,
+    app = require('connect')(),
     serveStatic = require('serve-static');
    
 if (env === 'production') {
-    app.use(serveStatic('./dist'));
+    app.use(serveStatic('dist'));
 } else {
-    app.use(serveStatic('./app'));
+    app
+        .use(require('connect-livereload')({ port: 35729 }))
+        .use(serveStatic('app'))
+        .use(serveStatic('.tmp'));
 }
 
-app.set('port', process.env.PORT || 4000);
-app.listen(app.get('port'), function() {
-    console.log('Server started on port %d', app.get('port'));
+app.listen(port, function() {
+    console.log('Web server started on port %d', port);
 });
